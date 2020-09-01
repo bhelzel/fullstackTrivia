7c69fa8c70a16f766e30f702d852422bc45657c0
Barrett Helzel <barrett.p.helzel@gmail.com>
2020-09-01 12:38:14 -0700

    setup database

--- db/migrate/20200831171118_create_questions.rb
+++ db/migrate/20200831171118_create_questions.rb
@@ -1,7 +1,13 @@
 class CreateQuestions < ActiveRecord::Migration[5.2]
   def change
     create_table :questions do |t|
-
+      t.string :text
+      t.string :answer
+      t.string :category
+      t.string :dummy_answers, array: true, default: []
+      t.string :difficulty
+    
+      t.timestamps
       t.timestamps
     end
   end
